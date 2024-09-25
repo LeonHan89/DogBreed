9322e12d7180d14c312264c096264c2538c232bc
Leon <tommy-online@163.com>
2024-09-25 16:25:22 +0800

    include pods

---
+++ DogBreedGuessing/Pods/Target Support Files/Pods-DogBreedGuessing-DogBreedGuessingUITests/Pods-DogBreedGuessing-DogBreedGuessingUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DogBreedGuessing_DogBreedGuessingUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DogBreedGuessing_DogBreedGuessingUITests
+@end
---
+++ DogBreedGuessing/Pods/Target Support Files/Pods-DogBreedGuessing/Pods-DogBreedGuessing-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DogBreedGuessing : NSObject
+@end
+@implementation PodsDummy_Pods_DogBreedGuessing
+@end
